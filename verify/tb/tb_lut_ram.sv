63098a4ac1025056f3270031005ab3737b153d6d
woodrowb <brenwoodr96@gmail.com>
2026-02-14 02:48:17 -0800

    LUT_RAM_TB: added drive and monitor tasks to tb

--- verify/tb/tb_lut_ram.sv
+++ verify/tb/tb_lut_ram.sv
@@ -1,5 +1,6 @@
 import riscv_32i_defs_pkg::*;
 import lut_ram_verify_pkg::*;
+import tb_lut_ram_stimulus_pkg::*;
 
 module tb_lut_ram();
   localparam CLK_PERIOD = 10;
@@ -23,22 +24,43 @@ module tb_lut_ram();
               .rd_data(intf.rd_data)
               );
 
+  /********** TASKS ***********/
+
+  task drive(lut_ram_trans trans);
+    intf.wr_en <= trans.wr_en;
+    intf.wr_addr <= trans.wr_addr;
+    intf.rd_addr <= trans.rd_addr;
+    intf.wr_data <= trans.wr_data;
+  endtask
+
+  task monitor(lut_ram_trans trans);
+    trans.rd_data = intf.rd_data;
+  endtask
+
+  /************  TESTING ********/
+  lut_ram_trans trans;
   initial begin
-    intf.wr_en <= '0;
-    intf.wr_addr <= 'd1;
-    intf.rd_addr <= 'd1;
-    intf.wr_data <= '0;
+
+    trans = new();
+
+    trans.wr_en = '0;
+    trans.wr_addr <= 'd1;
+    trans.rd_addr <= 'd1;
+    trans.wr_data <= '0;
+    drive(trans);
 
     for(int i = 0; i < 10; i++) begin
       @(posedge intf.clk)
-      intf.wr_data <= intf.wr_data + 'd1;
-      intf.wr_en <= ~intf.wr_en;
-      intf.wr_addr <= i * 10;
-      intf.rd_addr <= i * 10;
+      trans.wr_data <= trans.wr_data + 'd1;
+      trans.wr_en <= ~trans.wr_en;
+      trans.wr_addr <= i * 10;
+      trans.rd_addr <= i * 10;
+      drive(trans);
       @(posedge intf.clk)
       #1
+      monitor(trans);
       $display("wr_en: %b\n wr_addr: %d, wr_data: %h\nrd_addr: %d, rd_data: %h\n",
-                intf.wr_en, intf.wr_addr, intf.wr_data, intf.rd_addr, intf.rd_data);
+                trans.wr_en, trans.wr_addr, trans.wr_data, trans.rd_addr, trans.rd_data);
     end
 
     $stop(1);
