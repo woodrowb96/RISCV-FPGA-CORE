549331cf764659984609861dec441683665cf3ec
woodrowb <brenwoodr96@gmail.com>
2026-02-19 17:08:56 -0800

    DATA_MEM_TB: add monitor modport to interfave

--- verify/interface/data_mem_intf.sv
+++ verify/interface/data_mem_intf.sv
@@ -8,6 +8,8 @@ interface data_mem_intf(input logic clk);
   word_t wr_data;
   word_t rd_data;
 
+  modport monitor(input clk, wr_sel, addr, wr_data, rd_data);
+
   function void print(string msg = "");
     $display("[%s] t=%0t wr_sel:%0b addr:%0d wr_data:%h rd_data:%h",
              msg, $time, wr_sel, addr, wr_data, rd_data);
