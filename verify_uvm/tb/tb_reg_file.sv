00d8c6a6f550d25b050395cb4ec8bba749b485dc
woodrowb <brenwoodr96@gmail.com>
2026-05-13 09:41:04 -0700

    TB_REG_FILE: integrate rand_test into uvm tb

---
+++ verify_uvm/tb/tb_reg_file.sv
@@ -0,0 +1,43 @@
+
+module tb_reg_file
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  import rv32i_verify_pkg::*;
+  import reg_file_tests_pkg::*;
+();
+  /*********** CLK *************/
+  bit clk;
+  initial begin
+    clk = 0;
+    forever #(CLK_PERIOD/2) clk = ~clk;
+  end
+
+  /*********** INTERFACE *************/
+  reg_file_intf intf(.clk);
+
+  /*********** DUT *************/
+  reg_file dut(.clk(clk),
+              .wr_en(intf.wr_en),
+              .rd_reg_1(intf.rd_reg_1),
+              .rd_reg_2(intf.rd_reg_2),
+              .wr_reg(intf.wr_reg),
+              .wr_data(intf.wr_data),
+              .rd_data_1(intf.rd_data_1),
+              .rd_data_2(intf.rd_data_2));
+
+  /*********** BIND ASSERTIONS *************/
+  // bind tb_reg_file.dut reg_file_assert dut_assert(.*);
+
+  /************ COVERAGE *******************/
+  // tb_reg_file_coverage coverage;
+
+  /**************  TESTING ***************************/
+
+  initial begin
+    // coverage = new();
+    uvm_config_db#(virtual reg_file_intf)::set(null, "uvm_test_top", "reg_file_vif", intf);
+    run_test("reg_file_rand_test");
+    $stop(1);
+  end
+endmodule
