7ab5206ff1d00651acb1c3160cdaf5241590719d
woodrowb <brenwoodr96@gmail.com>
2026-05-16 15:46:44 -0700

    TB_DATA_MEM: integrate uvm tests into a tb and add testlist for regression

--- scripts/xsim/filelist/tb_data_mem.f
+++ scripts/xsim/filelist/tb_data_mem.f
@@ -1,13 +1,3 @@
-#LIB
-lib/base_transaction_pkg.sv
-lib/base_generator_pkg.sv
-lib/base_driver_pkg.sv
-lib/base_monitor_pkg.sv
-lib/base_scoreboard_pkg.sv
-lib/base_predictor_pkg.sv
-lib/base_reset_detector_pkg.sv
-lib/base_test_pkg.sv
-
 # RTL
 rtl/common/rv32i_defs_pkg.sv
 rtl/common/rv32i_control_pkg.sv
@@ -15,19 +5,12 @@ rtl/common/rv32i_config_pkg.sv
 rtl/lut_ram.sv
 rtl/data_mem.sv
 
-# Verification
-verify/common/verify_const_pkg.sv
-verify/interface/data_mem_intf.sv
-verify/assert/lut_ram_assert.sv
-verify/assert/data_mem_assert.sv
-verify/transaction/tb_lut_ram_transaction_pkg.sv
-verify/transaction/tb_data_mem_transaction_pkg.sv
-verify/ref_model/lut_ram_ref_model_pkg.sv
-verify/ref_model/data_mem_ref_model_pkg.sv
-verify/coverage/tb_data_mem_coverage_pkg.sv
-verify/generator/tb_data_mem_generator_pkg.sv
-verify/driver/tb_data_mem_driver_pkg.sv
-verify/monitor/tb_data_mem_monitor_pkg.sv
-verify/predictor/tb_data_mem_predictor_pkg.sv
-verify/scoreboard/tb_data_mem_scoreboard_pkg.sv
-verify/tests/tb_data_mem_tests_pkg.sv
+#Verification
+verify_uvm/common/rv32i_verify_pkg.sv
+verify_uvm/interface/data_mem_intf.sv
+verify_uvm/agents/data_mem/data_mem_agent_pkg.sv
+verify_uvm/ref_model/data_mem/data_mem_ref_model_pkg.sv
+verify_uvm/coverage/data_mem/data_mem_coverage_pkg.sv
+verify_uvm/env/data_mem/data_mem_env_pkg.sv
+verify_uvm/sequences/data_mem/data_mem_seq_pkg.sv
+verify_uvm/tests/data_mem/data_mem_tests_pkg.sv
---
+++ verify_uvm/tb/tb_data_mem.sv
@@ -0,0 +1,33 @@
+module tb_data_mem
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  import rv32i_verify_pkg::*;
+  import data_mem_tests_pkg::*;
+();
+
+  /*********** CLK *************/
+  bit clk;
+  initial begin
+    clk = 0;
+    forever #(CLK_PERIOD / 2) clk = ~clk;
+  end
+
+  /*********** INTERFACE *************/
+  data_mem_intf intf(.clk);
+
+  /*********** DUT *************/
+  data_mem dut(.clk(clk),
+               .wr_sel(intf.wr_sel),
+               .addr(intf.addr),
+               .wr_data(intf.wr_data),
+               .rd_data(intf.rd_data)
+               );
+
+  /**************  TESTING ***************************/
+  initial begin
+    uvm_config_db#(virtual data_mem_intf)::set(null, "uvm_test_top", "data_mem_vif", intf);
+    run_test();
+    $stop(1);
+  end
+endmodule
