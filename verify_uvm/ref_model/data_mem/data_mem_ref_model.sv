fd3fafb1fbd4e7ffb39ecd6a4db8574527cdcb6c
woodrowb <brenwoodr96@gmail.com>
2026-05-16 15:36:51 -0700

    TB_DATA_MEM: add ref_model

---
+++ verify_uvm/ref_model/data_mem/data_mem_ref_model.sv
@@ -0,0 +1,43 @@
+/*
+  NOTE: Out Of Bounds Access
+    - The rtl silently wraps out-of-bounds reads/writes to the start of memory.
+      This reference model mirrors that behavior via modular arithmetic on the
+      byte indices.
+*/
+class data_mem_ref_model extends uvm_object;
+  `uvm_object_utils(data_mem_ref_model);
+
+  //byte-addressable so total byte count is DEPTH(words) * 4
+  localparam int unsigned REF_MEM_DEPTH = DATA_MEM_DEPTH * 4;
+  typedef logic [$clog2(REF_MEM_DEPTH)-1:0] ref_mem_addr_t;
+
+  byte_t mem [0:REF_MEM_DEPTH-1];
+
+  function new(string name = "data_mem_ref_model");
+    super.new(name);
+  endfunction
+
+  function word_t read(word_t addr);
+    //add the byte offset to each byte, wrap via % to mirror the RTL's
+    //silent OOB wrap, then truncate to ref_mem_addr_t width
+    ref_mem_addr_t byte_3 = ref_mem_addr_t'((addr + 'd3) % REF_MEM_DEPTH);
+    ref_mem_addr_t byte_2 = ref_mem_addr_t'((addr + 'd2) % REF_MEM_DEPTH);
+    ref_mem_addr_t byte_1 = ref_mem_addr_t'((addr + 'd1) % REF_MEM_DEPTH);
+    ref_mem_addr_t byte_0 = ref_mem_addr_t'((addr + 'd0) % REF_MEM_DEPTH);
+
+    return {mem[byte_3], mem[byte_2], mem[byte_1], mem[byte_0]};
+  endfunction
+
+  function void update(data_mem_seq_item item);
+    ref_mem_addr_t byte_3 = ref_mem_addr_t'((item.addr + 'd3) % REF_MEM_DEPTH);
+    ref_mem_addr_t byte_2 = ref_mem_addr_t'((item.addr + 'd2) % REF_MEM_DEPTH);
+    ref_mem_addr_t byte_1 = ref_mem_addr_t'((item.addr + 'd1) % REF_MEM_DEPTH);
+    ref_mem_addr_t byte_0 = ref_mem_addr_t'((item.addr + 'd0) % REF_MEM_DEPTH);
+
+    //look at wr_sel and write the proper bytes
+    if(item.wr_sel[3]) mem[byte_3] = item.wr_data[31:24];
+    if(item.wr_sel[2]) mem[byte_2] = item.wr_data[23:16];
+    if(item.wr_sel[1]) mem[byte_1] = item.wr_data[15:8];
+    if(item.wr_sel[0]) mem[byte_0] = item.wr_data[7:0];
+  endfunction
+endclass
---
+++ verify_uvm/ref_model/data_mem/data_mem_ref_model_pkg.sv
@@ -0,0 +1,12 @@
+package data_mem_ref_model_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  import rv32i_defs_pkg::*;
+  import rv32i_config_pkg::*;
+  import rv32i_verify_pkg::*;
+
+  import data_mem_agent_pkg::*;
+
+  `include "data_mem_ref_model.sv"
+endpackage
