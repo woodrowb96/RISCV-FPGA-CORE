73ee2f07d77b0ba9f5404f562e11ad195f43231b
woodrowb <brenwoodr96@gmail.com>
2026-05-24 14:07:54 -0700

    ID_STAGE: add suffix to each signal in the interface

--- rtl/id_stage.sv
+++ rtl/id_stage.sv
@@ -5,25 +5,25 @@ CLK:
   - synchronous operations are synced to the posedge of the clk
 
 CONTROL:
-  - logic wr_en: 1 bit wr_en
+  - logic wr_en_wb: 1 bit wr_en
       - Controls when the reg_file is written to
       - When 1: wr_data is written into wr_reg @(posedge clk)
       - When 0: wr_data is not written
 
 INPUT:
-  - word_t inst: 32bit instruction from the if_stage
+  - word_t inst_if: 32bit instruction from the if_stage
 
-  - word_t wr_data: data which is to be written into the wr_reg
+  - word_t wr_data_if: data which is to be written into the wr_reg
       - This signal is routed back into the ID stage from the WB stage
 
 OUTPUT:
-  - word_t rs1_data: data read from the rs1 source register
+  - word_t rs1_data_id: data read from the rs1 source register
       - The rs1 register is parsed from bits [19:15] in the inst
 
-  - word_t rs2_data: data read from the rs2 source register
+  - word_t rs2_data_id: data read from the rs2 source register
       - The rs2 register is parsed from bits [24:20] in the inst
 
-  - word_t imm: 32 bit immediate encoded in the instruction
+  - word_t imm_id: 32 bit immediate encoded in the instruction
 */
 module id_stage
   import rv32i_defs_pkg::*;
@@ -32,16 +32,16 @@ module id_stage
   input logic clk,
 
   //control
-  input logic wr_en,
+  input logic wr_en_wb,
 
   //input
-  input word_t    inst,
-  input word_t    wr_data,
+  input word_t    inst_if,
+  input word_t    wr_data_if,
 
   //output
-  output word_t    rs1_data,
-  output word_t    rs2_data,
-  output word_t    imm
+  output word_t    rs1_data_id,
+  output word_t    rs2_data_id,
+  output word_t    imm_id
 );
-endmodule
 
+endmodule
