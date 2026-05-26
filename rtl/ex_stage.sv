db78c4f414ac0f3a9faf5fba4865b0828468a35b
woodrowb <brenwoodr96@gmail.com>
2026-05-26 14:29:00 -0700

    ex_stage: add branch_type control to the ex_stage interface

--- rtl/ex_stage.sv
+++ rtl/ex_stage.sv
@@ -3,19 +3,20 @@ module ex_stage
   import rv32i_control_pkg::*;
 (
   //control
-  input alu_op_t        alu_op_id,
-  input alu_src_sel_t   alu_src_sel_id,
+  input alu_op_t      alu_op_id,
+  input alu_src_sel_t alu_src_sel_id,
+  input branch_type_t branch_type_id,
 
   //input
   input word_t pc_id,
   input word_t imm_id,
   input word_t rs1_data_id,
-  input word_t rs2_data_id,
+  input word_t rs2_data_id
 
   //output
-  output word_t rs2_data_ex,
-  output word_t alu_result_ex,
   output logic  branch_taken_ex,
-  output word_t branch_target_ex
+  output word_t branch_target_ex,
+  output word_t alu_result_ex,
+  output word_t rs2_data_ex
 );
 endmodule
