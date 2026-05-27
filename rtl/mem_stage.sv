0d55550856cf9e631d90382e170c3535593e5827
woodrowb <brenwoodr96@gmail.com>
2026-05-27 16:33:17 -0700

    MEM_STAGE: add implementation

--- rtl/mem_stage.sv
+++ rtl/mem_stage.sv
@@ -1,12 +1,11 @@
 module mem_stage
   import rv32i_defs_pkg::*;
-  import rv32i_config_pkg::*;
   import rv32i_control_pkg::*;
 (
   input logic clk,
 
   //control
-  input byte_sel_t mem_wr_byte_sel_ex,
+  input byte_sel_t mem_store_byte_sel_ex,
 
   //input
   input word_t alu_result_ex,
@@ -16,4 +15,15 @@ module mem_stage
   output word_t alu_result_mem,
   output word_t load_data_mem
 );
+  /************ PASS THROUGHS ***********/
+  assign alu_result_mem = alu_result_ex;
+
+  /************** DATA MEMORY************/
+  data_mem u_data_mem (
+    .clk(clk),
+    .store_byte_sel(mem_store_byte_sel_ex),
+    .addr(alu_result_ex),
+    .store_data(rs2_data_ex),
+    .load_data(load_data_mem)
+  );
 endmodule
