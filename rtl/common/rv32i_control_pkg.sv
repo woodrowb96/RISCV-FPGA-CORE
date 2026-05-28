7bb99dbd34a0ecf3823688dfdedbfc403b2f8f7e
woodrowb <brenwoodr96@gmail.com>
2026-05-28 12:37:14 -0700

    COMMON: add OP_REG f3 params

--- rtl/common/rv32i_control_pkg.sv
+++ rtl/common/rv32i_control_pkg.sv
@@ -2,6 +2,8 @@
   This module contains typedefs and params related to implementation specific control.
 */
 package rv32i_control_pkg;
+  import rv32i_defs_pkg::*;
+
   /*********************** BYTE SELECT ************************/
   //select sub bytes in a word
   //for example in data_mem we use it to select which bytes to write
@@ -47,4 +49,14 @@ package rv32i_control_pkg;
     PC  = 1'b0,
     RS1 = 1'b1
   } branch_target_src_sel_t;
+
+  /***************** OP_REG F3 PARAMS ***********************/
+  localparam f3_t F3_ADD_SUB = 3'b000;
+  localparam f3_t F3_SLL     = 3'b001;
+  localparam f3_t F3_SLT     = 3'b010;
+  localparam f3_t F3_SLTU    = 3'b011;
+  localparam f3_t F3_XOR     = 3'b100;
+  localparam f3_t F3_SRL_SRA = 3'b101;
+  localparam f3_t F3_OR      = 3'b110;
+  localparam f3_t F3_AND     = 3'b111;
 endpackage
