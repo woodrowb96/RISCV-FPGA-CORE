725651abe495fc915eb69c7d41117926c0fc09d2
woodrowb <brenwoodr96@gmail.com>
2026-05-28 14:19:51 -0700

    MEM_STAGE: add load b/h/w/bu/hu implementation

--- rtl/mem_stage.sv
+++ rtl/mem_stage.sv
@@ -5,7 +5,8 @@ module mem_stage
   input logic clk,
 
   //control
-  input byte_sel_t mem_store_byte_sel_ex,
+  input byte_sel_t  mem_store_byte_sel_ex,
+  input load_type_t mem_load_type_ex,
 
   //input
   input word_t alu_result_ex,
@@ -15,6 +16,8 @@ module mem_stage
   output word_t alu_result_mem,
   output word_t load_data_mem
 );
+  word_t raw_load_data;
+
   /************ PASS THROUGHS ***********/
   assign alu_result_mem = alu_result_ex;
 
@@ -24,6 +27,30 @@ module mem_stage
     .store_byte_sel(mem_store_byte_sel_ex),
     .addr(alu_result_ex),
     .store_data(rs2_data_ex),
-    .load_data(load_data_mem)
+    .load_data(raw_load_data)
   );
+
+  /*********** CALC LOAD DATA ***************/
+  always_comb begin
+    unique case(mem_load_type_ex)
+      LOAD_B: begin
+        load_data_mem = {{24{raw_load_data[7]}}, raw_load_data[7:0]};
+      end
+      LOAD_H: begin
+        load_data_mem = {{16{raw_load_data[15]}}, raw_load_data[15:0]};
+      end
+      LOAD_W: begin
+        load_data_mem = raw_load_data;
+      end
+      LOAD_BU: begin
+        load_data_mem = {24'b0, raw_load_data[7:0]};
+      end
+      LOAD_HU: begin
+        load_data_mem = {16'b0, raw_load_data[15:0]};
+      end
+      default: begin
+        load_data_mem = 'x;
+      end
+    endcase
+  end
 endmodule
