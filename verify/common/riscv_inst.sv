72500e42629f77e7d3b9b7c95a732ef0838788cc
woodrowb <brenwoodr96@gmail.com>
2026-05-31 19:13:58 -0700

    VERIFY_COMMON: flesh out riscv_inst with decoding, imm constraints and inst printing

--- verify/common/riscv_inst.sv
+++ verify/common/riscv_inst.sv
@@ -4,6 +4,7 @@ class riscv_inst extends uvm_object;
   rand f3_t         f3;
   rand f7_t         f7;
   rand logic [31:0] imm;
+  inst_label_t      inst_label; //the decoded inst label (i.e. ADD,JAL,FENCE ...) set during post_rand
 
   `uvm_object_utils_begin(riscv_inst)
     `uvm_field_enum(opcode_t, opcode, UVM_DEFAULT)
@@ -15,38 +16,137 @@ class riscv_inst extends uvm_object;
     `uvm_field_int (imm, UVM_DEFAULT | UVM_HEX)
   `uvm_object_utils_end
 
+  /***********************************************************************************/
+  /************************** CONSTRAINTS *******************************************/
+  /***********************************************************************************/
   constraint valid_f3_f7 {
-    opcode == OP_REG -> f3 inside {F3_ADD_SUB, F3_SLL, F3_SLT, F3_SLTU, F3_XOR,
-                                    F3_SRL_SRA, F3_OR, F3_AND};
-    opcode == OP_REG && f3 == F3_ADD_SUB -> f7 inside {F7_ADD, F7_SUB};
-    opcode == OP_REG && f3 == F3_SRL_SRA -> f7 inside {F7_SRL, F7_SRA};
-    opcode == OP_REG && !(f3 inside {F3_ADD_SUB, F3_SRL_SRA}) -> f7 == '0;
+    if (opcode == OP_REG) {
+      f3 inside {F3_ADD_SUB, F3_SLL, F3_SLT, F3_SLTU, F3_XOR, F3_SRL_SRA, F3_OR, F3_AND};
+      if      (f3 == F3_ADD_SUB) f7 inside {F7_ADD, F7_SUB};
+      else if (f3 == F3_SRL_SRA) f7 inside {F7_SRL, F7_SRA};
+      else                       f7 == '0;
+    }
 
-    opcode == OP_IMM -> f3 inside {F3_ADDI, F3_SLLI, F3_SLTI, F3_SLTIU, F3_XORI,
-                                    F3_SRLI_SRAI, F3_ORI, F3_ANDI};
-    opcode == OP_IMM && f3 == F3_SRLI_SRAI -> f7 inside {F7_SRLI, F7_SRAI};
-    opcode == OP_IMM && f3 == F3_SLLI -> f7 == '0;
+    if (opcode == OP_IMM) {
+      f3 inside {F3_ADDI, F3_SLLI, F3_SLTI, F3_SLTIU, F3_XORI, F3_SRLI_SRAI, F3_ORI, F3_ANDI};
+      if      (f3 == F3_SRLI_SRAI) f7 inside {F7_SRLI, F7_SRAI};
+      else if (f3 == F3_SLLI)      f7 == '0;
+    }
 
-    opcode == OP_LOAD -> f3 inside {F3_LB, F3_LH, F3_LW, F3_LBU, F3_LHU};
-
-    opcode == OP_STORE -> f3 inside {F3_SB, F3_SH, F3_SW};
-
-    opcode == OP_BRANCH -> f3 inside {F3_BEQ, F3_BNE, F3_BLT, F3_BGE, F3_BLTU, F3_BGEU};
-
-    opcode == OP_JALR -> f3 == 3'b00;
-
-    opcode == OP_FENCE -> f3 == 3'b00;
-
-    opcode == OP_SYSTEM -> f3 == 3'b00;
+    if (opcode == OP_LOAD)   f3 inside {F3_LB, F3_LH, F3_LW, F3_LBU, F3_LHU};
+    if (opcode == OP_STORE)  f3 inside {F3_SB, F3_SH, F3_SW};
+    if (opcode == OP_BRANCH) f3 inside {F3_BEQ, F3_BNE, F3_BLT, F3_BGE, F3_BLTU, F3_BGEU};
+    if (opcode == OP_JALR)   f3 == 3'b000;
+    if (opcode == OP_FENCE)  f3 == 3'b000;
+    if (opcode == OP_SYSTEM) f3 == 3'b000;
   }
 
   constraint valid_imm {
-    opcode == OP_BRANCH -> imm[0] == '0;
-    opcode == OP_JAL    -> imm[0] == '0;
+    if (opcode inside {OP_LOAD, OP_STORE, OP_JALR}) {
+      imm[31:12] == {20{imm[11]}};   // S-type: sign-extend imm[11]
+    }
+
+    if(opcode == OP_IMM && f3 inside {F3_SLLI, F3_SRLI_SRAI}) {
+      //per spec we shift using only the lowest 4 bits
+      //the upper 7 bits are set to f7
+      imm[11:5] == f7;
+    }
+    else {
+      imm[31:12] == {20{imm[11]}};   // I-type: sign-extend imm[11]
+    }
+
+    if (opcode == OP_BRANCH) {
+      imm[31:13] == {19{imm[12]}};   // B-type: sign-extend imm[12]
+      imm[0]     == '0;              //lowest bit is 0
+    }
+
+    if (opcode == OP_JAL) {
+      imm[31:21] == {11{imm[20]}};   // J-type: sign-extend imm[20]
+      imm[0]     == '0;              //lowest bit is 0
+    }
+
+    if (opcode inside {OP_LUI, OP_AUIPC}) {
+      imm[11:0] == '0;               // U-type: lowest 12 bits are zero
+    }
   }
 
+  /***********************************************************************************/
+  /******************************** METHODS *******************************************/
+  /***********************************************************************************/
   function new(string name = "riscv_inst");
     super.new(name);
+  endfunction
+
+  function void post_randomize();
+    inst_label = INST_INVALID;
+
+    unique case(opcode)
+      OP_REG: begin
+        unique case(f3)
+          F3_ADD_SUB: begin
+            if     (f7 == F7_ADD) inst_label = INST_ADD;
+            else if(f7 == F7_SUB) inst_label = INST_SUB;
+          end
+          F3_SLL:                 inst_label = INST_SLL;
+          F3_SLT:                 inst_label = INST_SLT;
+          F3_SLTU:                inst_label = INST_SLTU;
+          F3_XOR:                 inst_label = INST_XOR;
+          F3_SRL_SRA: begin
+            if     (f7 == F7_SRL) inst_label = INST_SRL;
+            else if(f7 == F7_SRA) inst_label = INST_SRA;
+          end
+          F3_OR:                  inst_label = INST_OR;
+          F3_AND:                 inst_label = INST_AND;
+        endcase
+      end
+      OP_IMM: begin
+        unique case(f3)
+          F3_ADDI:                 inst_label = INST_ADDI;
+          F3_SLLI:                 inst_label = INST_SLLI;
+          F3_SLTI:                 inst_label = INST_SLTI;
+          F3_SLTIU:                inst_label = INST_SLTIU;
+          F3_XORI:                 inst_label = INST_XORI;
+          F3_SRLI_SRAI: begin
+            if     (f7 == F7_SRLI) inst_label = INST_SRLI;
+            else if(f7 == F7_SRAI) inst_label = INST_SRAI;
+          end
+          F3_ORI:                  inst_label = INST_ORI;
+          F3_ANDI:                 inst_label = INST_ANDI;
+        endcase
+      end
+      OP_LOAD: begin
+        unique case(f3)
+          F3_LB:  inst_label = INST_LB;
+          F3_LH:  inst_label = INST_LH;
+          F3_LW:  inst_label = INST_LW;
+          F3_LBU: inst_label = INST_LBU;
+          F3_LHU: inst_label = INST_LHU;
+        endcase
+      end
+      OP_STORE: begin
+        unique case(f3)
+          F3_SB: inst_label = INST_SB;
+          F3_SH: inst_label = INST_SH;
+          F3_SW: inst_label = INST_SW;
+        endcase
+      end
+      OP_BRANCH: begin
+        unique case(f3)
+          F3_BEQ:  inst_label = INST_BEQ;
+          F3_BNE:  inst_label = INST_BNE;
+          F3_BLT:  inst_label = INST_BLT;
+          F3_BGE:  inst_label = INST_BGE;
+          F3_BLTU: inst_label = INST_BLTU;
+          F3_BGEU: inst_label = INST_BGEU;
+        endcase
+      end
+      OP_LUI:    inst_label = INST_LUI;
+      OP_AUIPC:  inst_label = INST_AUIPC;
+      OP_JAL:    inst_label = INST_JAL;
+      OP_JALR:   inst_label = INST_JALR;
+      OP_FENCE:  inst_label = INST_FENCE;
+      OP_SYSTEM: inst_label = INST_ECALL; //Not implemeneted yet
+    endcase
   endfunction
 
   function word_t to_word();
@@ -67,5 +167,40 @@ class riscv_inst extends uvm_object;
       OP_SYSTEM:        return {25'b0, opcode};
       default:          return '0;
     endcase
+  endfunction
+
+  //Strip INST_ label from inst_label enum and return the remaining riscv
+  //mnemonic as a string (i.e. INST_ADDI -> ADDI)
+  function string mnemonic();
+    string n = inst_label.name();
+    return n.substr(5, n.len()-1);
+  endfunction
+
+  virtual function string convert2string();
+    string asm;
+    unique case(opcode)
+      OP_REG:    asm = $sformatf("%s x%0d, x%0d, x%0d", mnemonic(), rd,  rs1, rs2);
+      OP_IMM: begin
+        unique case(f3)
+          F3_ADDI, F3_SLTI, F3_SLTIU:
+                 asm = $sformatf("%s x%0d, x%0d, %0d",   mnemonic(), rd, rs1, $signed(imm[11:0]));
+          F3_XORI, F3_ORI, F3_ANDI:
+                 asm = $sformatf("%s x%0d, x%0d, 0x%0h", mnemonic(), rd, rs1, imm[11:0]);
+          F3_SLLI, F3_SRLI_SRAI:
+                 asm = $sformatf("%s x%0d, x%0d, %0d",   mnemonic(), rd, rs1, imm[4:0]);
+        endcase
+      end
+      OP_LOAD:   asm = $sformatf("%s x%0d, %0d(x%0d)", mnemonic(), rd,  $signed(imm[11:0]), rs1);
+      OP_STORE:  asm = $sformatf("%s x%0d, %0d(x%0d)", mnemonic(), rs2, $signed(imm[11:0]), rs1);
+      OP_BRANCH: asm = $sformatf("%s x%0d, x%0d, %0d", mnemonic(), rs1, rs2, $signed(imm[12:0]));
+      OP_LUI,
+      OP_AUIPC:  asm = $sformatf("%s x%0d, 0x%0h",     mnemonic(), rd,  imm[31:12]);
+      OP_JAL:    asm = $sformatf("%s x%0d, %0d",       mnemonic(), rd,  $signed(imm[20:0]));
+      OP_JALR:   asm = $sformatf("%s x%0d, x%0d, %0d", mnemonic(), rd,  rs1, $signed(imm[11:0]));
+      OP_FENCE,
+      OP_SYSTEM: asm = mnemonic();
+      default:   asm = $sformatf("INVALID_OPCODE(%0h)", opcode);
+    endcase
+    return $sformatf("%s  [0x%08h]", asm, to_word());
   endfunction
 endclass
--- verify/common/riscv_inst_pkg.sv
+++ verify/common/riscv_inst_pkg.sv
@@ -3,6 +3,7 @@ package riscv_inst_pkg;
   `include "uvm_macros.svh"
 
   import rv32i_defs_pkg::*;
+  import rv32i_verify_pkg::*;
 
   `include "riscv_inst.sv"
 endpackage
